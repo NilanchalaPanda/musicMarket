babca519d0a7ff63548a5c3ac463b2fed2121ac9
atharvamulam <atharvamulam@gmail.com>
2025-05-01 22:21:26 +0530

    Split error fix

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,17 @@
+// export const GetIpfsUrlFromPinata = (pinataUrl) => {
+//     var IPFSUrl = pinataUrl.split("/");
+//     const lastIndex = IPFSUrl.length;
+//     IPFSUrl = "https://ipfs.io/ipfs/"+IPFSUrl[lastIndex-1];
+//     return IPFSUrl;
+// };
+
+//Updated code
 export const GetIpfsUrlFromPinata = (pinataUrl) => {
-    var IPFSUrl = pinataUrl.split("/");
-    const lastIndex = IPFSUrl.length;
-    IPFSUrl = "https://ipfs.io/ipfs/"+IPFSUrl[lastIndex-1];
-    return IPFSUrl;
+    if (!pinataUrl || typeof pinataUrl !== "string") {
+        console.warn("Invalid pinataUrl provided:", pinataUrl);
+        return ""; // return a default fallback URL
+    }
+
+    const parts = pinataUrl.split("/");
+    return "https://ipfs.io/ipfs/" + parts[parts.length - 1];
 };

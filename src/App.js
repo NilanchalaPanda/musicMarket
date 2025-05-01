f0f1799eb10e5ff4cfd8a40cb61efdfc524b1d37
NilanchalaPanda <nilanchalpanda2003@gmail.com>
2025-05-01 21:04:26 +0530

    fix: overflow fixes.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
+    <div className="w-full overflow-x-hidden">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Marketplace />} />
